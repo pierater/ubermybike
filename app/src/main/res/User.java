8916ad9e0c21fef27b7b91c06f2a0d49eedd7d12
Martin Almaraz <martin.almaraz@gmail.com>
2015-03-13 21:53:21 -0700

    Started doing the java classes

---
+++ app/src/main/res/User.java
@@ -0,0 +1,24 @@
+public class User {
+
+    public string name;
+    public Bike = bike; // still need to init this
+    public string phone_number;
+    public string email;
+    public int rating;
+    public int experience;
+
+    public User(string startname, string startphone_number, string startemail, Bike bike) {
+
+        name = startname;
+        phone_number = startphone_number;
+        email = startemail;
+        bike = new Bike;
+    }
+
+    public void rentBike() {
+
+
+
+
+    }
+}
