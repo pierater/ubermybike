11d294b98745ee9eacfac486fd9f6aada155f036
GabrielZapata <gabriel.pyramids@gmail.com>
2015-03-14 15:42:56 -0700

    added bike I.D. variable

--- app/src/main/java/com/example/idk/myuber/Bike.java
+++ app/src/main/java/com/example/idk/myuber/Bike.java
@@ -12,16 +12,17 @@ public class Bike {
    private Boolean isAvailable;
    private String lat;//coords latitude
    private String lon;//coords longitude
-
+   private String bike_id;// a string because we are not doing math with it. it also represents the bike as an identification
 
    //bike class constructor takes in parameters from the inputs of the user to be stored... somewhere.
 
-    public Bike(int bike_rating, String owner_name, Boolean isAvailable, String lat, String lon){
+    public Bike(int bike_rating, String owner_name, Boolean isAvailable, String lat, String lon, String bike_id){
         this.bike_rating = bike_rating;
         this.owner_name = owner_name;
         this.isAvailable = isAvailable;
         this.lat = lat;
         this.lon = lon;
+        this.bike_id = bike_id;
     }
     //getters and setters for the method variables
     //NEED: figure out where to connect the the variables to.
@@ -35,6 +36,7 @@ public class Bike {
     public Boolean getIsAvailable(){ return isAvailable; }
     public String getLat() { return lat; }
     public String getLon() { return lon; }
+    public String getBike_id() { return bike_id; }
 
     public void setBike_rating(int bike_rating){
         this.bike_rating = bike_rating;
@@ -42,7 +44,9 @@ public class Bike {
     public void setOwner_name(String owner_name){
         this.owner_name = owner_name;
     }
-    public void setIsAvailable(Boolean isAvailable) {this.isAvailable = isAvailable;}
-    public void setLat(String lat) {this.lat = lat;}
-    public void setLon(String lon) {this.lon = lon;}
+    public void setIsAvailable(Boolean isAvailable) { this.isAvailable = isAvailable; }
+    public void setLat(String lat) { this.lat = lat; }
+    public void setLon(String lon) { this.lon = lon; }
+    public void setBike_id(String bike_id) { this.bike_id = bike_id; }
+
 }
