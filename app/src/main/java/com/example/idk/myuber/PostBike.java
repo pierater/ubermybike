f19a45e8d8bfecb9411a7b55681376e3fe0e8b01
chrisgin <christiandmartinez@student.hartnell.edu>
2015-03-14 20:52:33 -0700

    Fixed issues with the camera app working

--- app/src/main/java/com/example/idk/myuber/PostBike.java
+++ app/src/main/java/com/example/idk/myuber/PostBike.java
@@ -1,12 +1,14 @@
 package com.example.idk.myuber;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 
@@ -14,6 +16,10 @@ import org.json.JSONObject;
  * Created by Mikaami13 on 3/14/2015.
  */
 public class PostBike extends Fragment {
+    private int requestCode;
+    private int resultCode;
+    private Intent data;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //        test();
@@ -21,6 +27,31 @@ public class PostBike extends Fragment {
 
     }
 
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == getActivity().RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+                Toast.makeText(getActivity(), "Image saved to:\n" +
+                        intent.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == getActivity().RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+            }
+        }
+    }
 
     public void test() {
 
