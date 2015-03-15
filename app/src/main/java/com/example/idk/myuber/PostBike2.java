3750588257252cfbcdc19c19d0a82d2dafbc8d35
Mikaami <asoriano@csumb.edu>
2015-03-15 08:47:14 -0700

    New Method

--- app/src/main/java/com/example/idk/myuber/PostBike2.java
+++ app/src/main/java/com/example/idk/myuber/PostBike2.java
@@ -3,6 +3,7 @@ package com.example.idk.myuber;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,8 +11,12 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Created by idk on 3/14/15.
@@ -29,9 +34,24 @@ public class PostBike2 extends Activity {
         imgview.setImageURI(imgUri);
         TextView txtview =(TextView)findViewById(R.id.post_description_string);
         txtview.setText(description);
+        Button postBike = (Button)findViewById(R.id.post_bike_post_button);
+        postBike.setOnClickListener((View.OnClickListener) this);
 
     }
     protected void onDestroy(){
         super.onDestroy();
     }
+
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.post_bike_post_button:
+                Httpget post = new Httpget();
+                JSONObject postobj = new JSONObject();
+                AsyncTask<String, Void, Void> task = post.new newBike();
+                task.execute("1");
+                break;
+        }
+
+    }
+
 }
